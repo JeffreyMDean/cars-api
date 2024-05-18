511a68c439cb285388259cc07136d6b6d3ea3403
JeffreyMDean <jeffrey.michael.dean@gmail.com>
2024-05-18 15:55:15 -0400

    Added create route/controller

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -3,4 +3,13 @@ class CarsController < ApplicationController
     @cars = Car.all
     render template: "cars/index"
   end
+
+  def create
+    @car = Car.create(
+      car_make: params[:car_make],
+      car_model: params[:car_model],
+      year: params[:year]
+    )
+    render :show
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   get "/cars" => "cars#index"
+  post "/cars" => "cars#create"
 end
